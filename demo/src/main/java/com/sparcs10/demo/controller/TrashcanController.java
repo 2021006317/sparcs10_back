b181176ca486271b3020960a8d7940f31187326a
2021006317 <eve7777@hanyang.ac.kr>
2024-07-27 20:15:43 +0900

    FEAT: response 형태 통일 - Trashcan

--- demo/src/main/java/com/sparcs10/demo/controller/TrashcanController.java
+++ demo/src/main/java/com/sparcs10/demo/controller/TrashcanController.java
@@ -2,7 +2,10 @@ package com.sparcs10.demo.controller;
 
 import com.sparcs10.demo.dto.TrashcanDTO;
 import com.sparcs10.demo.service.TrashcanService;
+import com.sparcs10.demo.utils.CustomResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +19,10 @@ public class TrashcanController {
     private final TrashcanService trashcanService;
 
     @GetMapping("/list")
-    public List<TrashcanDTO> list() {
-        return trashcanService.list();
+    public ResponseEntity<CustomResponse<List<TrashcanDTO>>> list() {
+        return new ResponseEntity<>(
+                CustomResponse.response(trashcanService.list()),
+                HttpStatus.OK
+        );
     }
 }
